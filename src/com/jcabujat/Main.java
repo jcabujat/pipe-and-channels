8713b10c0299549b1c0046107a94eff06e15749f
jonathancabujat <jonathancabujat@gmail.com>
2020-10-14 10:51:48 +0800

    01: Code for reading and writing files that are accessible between threads. It uses Pipe, SinkChannel for writing and SourceChannel for reading.

---
+++ src/com/jcabujat/Main.java
@@ -0,0 +1,63 @@
+package com.jcabujat;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Pipe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            Pipe pipe = Pipe.open();
+
+            Runnable writer = () -> {
+                try {
+                    Pipe.SinkChannel sinkChannel = pipe.sink();
+                    ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                    for (int i = 0; i < 10; i++) {
+                        String currentTime = "The current time is " + System.currentTimeMillis();
+
+                        buffer.put(currentTime.getBytes());
+                        buffer.flip();
+
+                        while (buffer.hasRemaining()) {
+                            sinkChannel.write(buffer);
+                        }
+                        buffer.flip();
+                        Thread.sleep(100);
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            };
+
+            Runnable reader = () -> {
+                try {
+                    Pipe.SourceChannel sourceChannel = pipe.source();
+                    ByteBuffer buffer = ByteBuffer.allocate(56);
+
+                    for(int i=0;i<10;i++){
+                        int bytesRead = sourceChannel.read(buffer);
+                        byte[] timeString = new byte[bytesRead];
+                        buffer.flip();
+                        buffer.get(timeString);
+                        System.out.println("Reader thread " + new String(timeString));
+                        buffer.flip();
+                        Thread.sleep(100);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            };
+
+            new Thread(writer).start();
+            new Thread(reader).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
